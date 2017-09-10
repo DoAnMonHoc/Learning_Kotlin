bfc9d3c654ddfeb211f2670a608c722d5f9e0ae8
PaulDuong <vanlocduong1996@gmail.com>
2017-09-11 06:07:22 +0700

    First Project Kotlin

---
+++ MyApplication/app/src/main/java/com/paulduong/demo_first_kotlin/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.paulduong.demo_first_kotlin
+
+import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.view.View
+import android.view.Menu
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.content_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val toolbar = findViewById(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
+
+        val fab = findViewById(R.id.fab) as FloatingActionButton
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+
+        btntinh.setOnClickListener(View.OnClickListener {
+            Process_Caculate()
+
+        })
+
+
+
+
+    }
+
+    private  fun  Process_Caculate(){
+        val a:Int= edt_soa.text.toString().toInt()
+        val b:Int= edt_soa.text.toString().toInt()
+        val  ketqua:Int=a + b
+        txt_ketqua.text= ketqua.toString()
+    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        val id = item.itemId
+
+
+        return if (id == R.id.action_settings) {
+            true
+        } else super.onOptionsItemSelected(item)
+
+    }
+}
